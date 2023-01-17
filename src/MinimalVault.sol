a675d4441cf5c91d23287950469e6f4c149e83e2
Théo RICHARD <mail@rcode.tech>
2023-01-17 17:17:30 +0100

    feat: minimal data, event and functions prototypes

--- src/MinimalVault.sol
+++ src/MinimalVault.sol
@@ -3,5 +3,22 @@ pragma solidity ^0.8.13;
 
 // solhint-disable-next-line no-empty-blocks
 contract MinimalVault {
+    /*//////////////////////////////////////////////////////////////
+                                USER DATA
+    //////////////////////////////////////////////////////////////*/
+    mapping(address => uint256) public depositsOf;
+    uint256 public totalETHDeposited;
 
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event Deposit(address indexed depositor, uint256 indexed amount);
+    event Withdraw(address indexed depositor, uint256 indexed amount);
+
+    /*//////////////////////////////////////////////////////////////
+                        DEPOSIT/WITHDRAWAL LOGIC
+    //////////////////////////////////////////////////////////////*/
+    function deposit() public payable {}
+
+    function withdraw() public {}
 }
